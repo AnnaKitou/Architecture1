f8bcb574897fe378031e0174e63fe85c630375c3
Christina Adams <chrystal_7_1@hotmail.com>
2022-02-23 21:57:30 +0200

    Penguin

---
+++ Entities/Animal/Penguin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Animal
+{
+    public class Penguin
+    {
+        public class Penguin
+        {
+            public int PenguinId { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
