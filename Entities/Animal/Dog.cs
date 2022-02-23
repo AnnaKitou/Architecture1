beb74abc578964a6af4718a1d325acbcb4278260
Christos Karamolegkos <c.karamolegkos@gmail.com>
2022-02-23 21:31:28 +0200

    class dog

---
+++ Entities/Animal/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Animal
+{
+    public class Dog
+    {
+        public int DogId { get; set; }
+        public string DogName { get; set; }
+    }
+}
