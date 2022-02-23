0d2c2c99a0c786ec6d7bcb792b6bd00b3b5d63d3
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-02-23 21:28:49 +0200

    Folder

---
+++ Entities/Animal/Tiger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Animal
+{
+    public class Tiger
+    {
+        public int TigerId { get; set; } 
+        public string TigerName { get; set; }    
+
+
+    }
+}
